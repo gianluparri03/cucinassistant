c80ec62e783d104270419a009dbcb26e76db5210
gianluparri03 <gianlucaparri03@gmail.com>
2025-04-22 22:58:13 +0200

    Simplified makefile

--- src/database/main_test.go
+++ src/database/main_test.go
@@ -1,25 +1,78 @@
 package database
 
 import (
+	"database/sql"
 	"log/slog"
 	"os"
+	"strings"
 	"testing"
 
 	"cucinassistant/configs"
 )
 
+// testingDBName is the name of the database where the tests will be run.
+// It is set to the database name (see configs.Database) with a _test suffix.
+// The database is created and dropped at every test.
+var testingDBName string
+
+// testingOrigConn is used to hold the connection to the default database
+// when connecting to the testing one.
+var testingOrigConn *sql.DB
+
 // TestMain sets up the testing environment
 func TestMain(m *testing.M) {
 	slog.SetLogLoggerLevel(slog.LevelError)
-	os.Chdir("..")
 
-	// Loads the configuration
+	os.Chdir("..")
 	configs.LoadAndParse()
 
-	// Connects to the database and do the bootstrap
-	Connect()
-	Bootstrap()
+	openTestDB()
+	defer closeTestDB()
 
-	// Runs the actual tests
+	Bootstrap()
 	m.Run()
+}
+
+// openTestDB creates a testing database
+func openTestDB() {
+	var dbName string
+
+	// Builds the testing db's name
+	for _, pair := range strings.Split(configs.Database, " ") {
+		if strings.HasPrefix(pair, "dbname=") {
+			dbName = pair[len("dbname="):]
+			break
+		}
+	}
+
+	// Connects to the original database
+	Connect()
+	testingOrigConn = db
+
+	// Creates the testing db
+	testingDBName = dbName + "_test"
+	_, err := db.Exec("CREATE DATABASE " + testingDBName + ";")
+	if err != nil {
+		slog.Error("while creating testing db:", "err", err)
+		os.Exit(1)
+	}
+
+	// Connects to the testing database
+	configs.Database = strings.ReplaceAll(configs.Database,
+		"dbname="+dbName,
+		"dbname="+testingDBName)
+	Connect()
+}
+
+// closeTestDB drops the testing database
+func closeTestDB() {
+	db.Close()
+	db = testingOrigConn
+
+	// Drops the testing database
+	_, err := db.Exec("DROP DATABASE " + testingDBName + ";")
+	if err != nil {
+		slog.Error("while dropping testing db:", "err", err)
+		os.Exit(1)
+	}
 }
